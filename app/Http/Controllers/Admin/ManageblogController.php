cb711d8fadff77e54fc2c61e63c11b294573b877
SatishDash20 <44016883+SatishDash20@users.noreply.github.com>
2024-07-10 23:08:10 +0530

    blog filter

--- app/Http/Controllers/Admin/ManageblogController.php
+++ app/Http/Controllers/Admin/ManageblogController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\BlogCategory;
+use App\Models\BlogTag;
 use Illuminate\Http\Request;
 use App\Models\Blog;
 use Illuminate\Support\Facades\DB;
@@ -10,18 +12,20 @@ use Illuminate\Support\Facades\View;
 
 class ManageblogController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         return view('admin.manageblogs.add');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
 
         $catid = $request->get('category');
         $catslug = DB::table('blog_categories')->select('slug')->where('cat_id', $catid)->value('slug');
-        
+
         $tagid = $request->get('tag');
         $tagslug = DB::table('blog_tags')->select('slug')->where('tag_id', $tagid)->value('slug');
-        
+
         $blogdata = new Blog();
         $blogdata->cat_slug = $catslug; // Assuming 'cat_slug' is a string field
         $blogdata->tag_slug = $tagslug; // Assuming 'tag_slug' is a string field
@@ -33,46 +37,48 @@ class ManageblogController extends Controller
         $blogdata->meta_desc = $request->input('meta_description');
         $blogdata->is_active = $request->input('status');
         $blogdata->created_at = now();
-        $blogdata->updated_at = now(); 
-        
+        $blogdata->updated_at = now();
+
         if ($request->hasFile('image')) {
             // Generate a unique file name for the image
-            $imageName = 'blogs_' . time() .'.'.$request->file('image')->getClientOriginalExtension();
-            
+            $imageName = 'blogs_' . time() . '.' . $request->file('image')->getClientOriginalExtension();
+
             $destinationDirectory = public_path('images/blogs');
-        
+
             if (!is_dir($destinationDirectory)) {
                 mkdir($destinationDirectory, 0777, true);
             }
-        
+
             // Move the file to the public/uploads directory
             $request->file('image')->move($destinationDirectory, $imageName);
-        
+
             $blogdata->blog_image = $imageName;
         }
-        
+
         $blogdata->save();
-        
+
         return back()->with('message', 'Blog Added Successfully!');
     }
 
-    public function edit($id){
-       
+    public function edit($id)
+    {
+
         $editblogs = Blog::find($id);
         // dd($editblogs);
-        $editblogcat=DB::table('blog_categories')->where('slug',$editblogs->cat_slug)->first();
-        $editblogtag=DB::table('blog_tags')->where('slug',$editblogs->tag_slug)->first();
-   
-        return view('admin.manageblogs.edit',compact('editblogs','editblogcat','editblogtag'));
+        $editblogcat = DB::table('blog_categories')->where('slug', $editblogs->cat_slug)->first();
+        $editblogtag = DB::table('blog_tags')->where('slug', $editblogs->tag_slug)->first();
+
+        return view('admin.manageblogs.edit', compact('editblogs', 'editblogcat', 'editblogtag'));
     }
 
-    public function update(Request $request,$id){
+    public function update(Request $request, $id)
+    {
         $catid = $request->get('category');
         $catslug = DB::table('blog_categories')->select('slug')->where('cat_id', $catid)->value('slug');
-        
+
         $tagid = $request->get('tag');
         $tagslug = DB::table('blog_tags')->select('slug')->where('tag_id', $tagid)->value('slug');
-        
+
         $blogdata = Blog::findOrFail($id);
         $blogdata->cat_slug = $catslug; // Assuming 'cat_slug' is a string field
         $blogdata->tag_slug = $tagslug; // Assuming 'tag_slug' is a string field
@@ -83,22 +89,22 @@ class ManageblogController extends Controller
         $blogdata->meta_key = $request->input('meta_key');
         $blogdata->meta_desc = $request->input('meta_description');
         $blogdata->is_active = $request->input('status');
-        $blogdata->updated_at = now(); 
-        
+        $blogdata->updated_at = now();
+
         // Update Image
         if ($request->hasFile('image')) {
             // Generate a unique file name for the image
-            $imageName = 'blogs_' . time() .'.'.$request->file('image')->getClientOriginalExtension();
-            
+            $imageName = 'blogs_' . time() . '.' . $request->file('image')->getClientOriginalExtension();
+
             $destinationDirectory = public_path('images/blogs');
-        
+
             if (!is_dir($destinationDirectory)) {
                 mkdir($destinationDirectory, 0777, true);
             }
-        
+
             // Move the file to the public/uploads directory
             $request->file('image')->move($destinationDirectory, $imageName);
-        
+
             // Delete existing image file if it exists
             if (!empty($blogdata->image)) {
                 $existingImagePath = $destinationDirectory . '/' . $blogdata->image;
@@ -106,24 +112,48 @@ class ManageblogController extends Controller
                     unlink($existingImagePath);
                 }
             }
-        
+
             $blogdata->blog_image = $imageName;
         }
-        
+
         $blogdata->save();
-        
+
         return back()->with('message', 'Blog updated Successfully !');
-        
+
 
     }
 
 
-    public function list(){
-        $blogs = Blog::all();
-        return view('admin.manageblogs.list',compact('blogs'));
+    public function list(Request $request)
+    {
+        $catSlug = $request->input('cat_slug');
+        $tagSlug = $request->input('tag_slug');
+        $searchTxt = $request->input('searchtxt');
+
+        $query = Blog::query();
+
+        if (!empty($catSlug)) {
+            $query->where('cat_slug', $catSlug);
+        }
+
+        if (!empty($tagSlug)) {
+            $query->where('tag_slug', $tagSlug);
+        }
+
+        if (!empty($searchTxt)) {
+            $query->where('page_name', 'like', '%' . $searchTxt . '%');
+        }
+
+        $blogs = $query->get();
+        $categories = BlogCategory::all();
+        $tags = BlogTag::all();
+
+        return view('admin.manageblogs.list', compact('blogs', 'categories', 'tags'));
     }
 
-    public function commentlist($id){
+
+    public function commentlist($id)
+    {
         dd($id);
 
     }
@@ -137,63 +167,64 @@ class ManageblogController extends Controller
 
         $blogs->delete(); // Delete the item
 
-         return redirect()->route('manageblog.list')->with('success', 'Blog deleted successfully.'); // Redirect to the index page with success message
-   }
-
-   public function filter(Request $request)
-{
-    $catSlug = $request->input('cat_slug');
-    $tagSlug = $request->input('tag_slug');
-    $searchTxt = $request->input('searchtxt');
-
-    $query = Blog::query();
-
-    if (!empty($catSlug)) {
-        $query->where('cat_slug', $catSlug);
+        return redirect()->route('manageblog.list')->with('success', 'Blog deleted successfully.'); // Redirect to the index page with success message
     }
 
-    if (!empty($tagSlug)) {
-        $query->where('tag_slug', $tagSlug);
+    public function filter(Request $request)
+    {
+        $catSlug = $request->input('cat_slug');
+        $tagSlug = $request->input('tag_slug');
+        $searchTxt = $request->input('searchtxt');
+
+        $query = Blog::query();
+
+        if (!empty($catSlug)) {
+            $query->where('cat_slug', $catSlug);
+        }
+
+        if (!empty($tagSlug)) {
+            $query->where('tag_slug', $tagSlug);
+        }
+
+        if (!empty($searchTxt)) {
+            $query->where('page_name', 'like', '%' . $searchTxt . '%');
+        }
+
+        $blogs = $query->get();
+
+        // Load the view and pass the filtered blogs data
+        $html = View::make('admin.manageblogs.list', compact('blogs'))->render();
+
+        return response()->json(['html' => $html]);
     }
 
-    if (!empty($searchTxt)) {
-        $query->where('page_name', 'like', '%' . $searchTxt . '%');
-    }
 
-    $blogs = $query->get();
-
-    // Load the view and pass the filtered blogs data
-    $html = View::make('admin.manageblogs.list', compact('blogs'))->render();
-
-    return response()->json(['html' => $html]);
-}
+    public function status(Request $request)
+    {
+        $get_id = $request->id;
+        $catstatus = DB::table('blogs')
+            ->select('is_active')
+            ->where('id', '=', $get_id)
+            ->first();
 
 
-public function status(Request $request){
-    $get_id=$request->id;
-    $catstatus=DB::table('blogs')
-    ->select('is_active')
-    ->where('id','=',$get_id)
-    ->first();
-    
+        $astatus = $catstatus->is_active;
+        if ($astatus == '1') {
+            $astatus = '0';
+        } else {
+            $astatus = '1';
+        }
+        $statusupdate = DB::table('blogs')
+            ->where('id', $get_id)
+            ->update(array('is_active' => $astatus));
 
-    $astatus=$catstatus->is_active;
-     if($astatus == '1'){
-         $astatus='0'; 
-     } else{
-         $astatus='1';
-     }
-     $statusupdate=DB::table('blogs')
-     ->where('id', $get_id)
-     ->update(array('is_active'=>$astatus));
-
-     if($statusupdate){
-         return response()->json([
-             'status' => 'success',
-             'code' => 200,
-         ]);
+        if ($statusupdate) {
+            return response()->json([
+                'status' => 'success',
+                'code' => 200,
+            ]);
         }
     }
 
-  
+
 }
